b3a890bb49e14add47acc8968314dedfb468f43b
laptop_martin <martin.fq1908@gmail.com>
2024-02-27 23:30:36 -0500

    Completado las pruebas de frecode

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,91 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {
+    let { coordinate, value, puzzle } = req.body;
+    let alphaRegex = /[a-i]/gi;
+    let numRegex = /^[1-9]$/g;
+    const regex = /^[1-9.]+$/;
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+    // GENERAL
+    if (!coordinate || !value || !puzzle) {
+      return res.json({ error: "Required field(s) missing" });
+    } else if (!regex.test(puzzle)) {
+      return res.json({ error: "Invalid characters in puzzle" });
+    } else if (puzzle.length !== 81) {
+      return res.json({ error: "Expected puzzle to be 81 characters long" });
+    }
 
-    });
+    // COORDINATE & VALUE
+    const splitCoord = coordinate.split("");
+    if (
+      splitCoord.length > 2 ||
+      splitCoord.length === 1 ||
+      !alphaRegex.test(splitCoord[0]) ||
+      !numRegex.test(splitCoord[1])
+    ) {
+      return res.json({ error: "Invalid coordinate" });
+    } else if (!value.match(numRegex)) {
+      return res.json({ error: "Invalid value" });
+    }
+
+    // COMPARE WITH PUZZLE
+    let rowPlace = solver.checkColPlacement(
+      puzzle,
+      splitCoord[0],
+      splitCoord[1],
+      value
+    );
+    let colPlace = solver.checkColPlacement(
+      puzzle,
+      splitCoord[0],
+      splitCoord[1],
+      value
+    );
+    let regionPlace = solver.checkRegionPlacement(
+      puzzle,
+      splitCoord[0],
+      splitCoord[1],
+      value
+    );
+    let errors = [];
+    if (rowPlace && colPlace && regionPlace) {
+      return res.json({ valid: true });
+    } else {
+      if (!rowPlace) {
+        errors.push("row");
+      }
+      if (!colPlace) {
+        errors.push("column");
+      }
+      if (!regionPlace) {
+        errors.push("region");
+      }
+      return res.json({ valid: false, conflict: errors });
+    }
+  });
+
+  app.route("/api/solve").post((req, res) => {
+    const { puzzle } = req.body;
+    const regex = /^[1-9.]+$/;
+
+    if (!puzzle) {
+      res.json({ error: "Required field missing" });
+    } else if (!regex.test(puzzle)) {
+      return res.json({ error: "Invalid characters in puzzle" });
+    } else if (puzzle.length !== 81) {
+      return res.json({ error: "Expected puzzle to be 81 characters long" });
+    }
+
+    let solvedString = solver.solve(puzzle);
+    if (!solvedString) {
+      res.json({ error: "Puzzle cannot be solved" });
+    } else {
+      return res.json({ solution: solvedString });
+    }
+  });
 };
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,102 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const Solver = require("../controllers/sudoku-solver.js");
+let solver = new Solver();
+let stringPuzzle =
+    "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
+let stringComplete =
+    "769235418851496372432178956174569283395842761628713549283657194516924837947381625";
 
-suite('Unit Tests', () => {
+suite("UnitTests", () => {
+    test("valid puzzle string of 81 chars", (done) => {
+        assert.equal(solver.solve(stringPuzzle), stringComplete);
+        assert.isTrue(solver.validate(stringPuzzle));
+        done();
+    });
 
+    test("puzzle string with invalid chars", (done) => {
+        assert.equal(
+            solver.solve(
+                "..9j.5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6.."
+            ),
+            false
+        );
+        assert.isFalse(
+            solver.validate(
+                "j.9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6.."
+            )
+        );
+        assert.isFalse(
+            solver.validate(
+                "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6.j"
+            )
+        );
+
+        done();
+    });
+
+    test("puzzle string that is not 81 char in length", (done) => {
+        assert.equal(
+            solver.solve(
+                "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6...."
+            ),
+            false
+        );
+        assert.isFalse(
+            solver.validate(
+                "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4"
+            )
+        );
+        done();
+    });
+
+    test("valid row placement", (done) => {
+        assert.isTrue(solver.checkRowPlacement(stringPuzzle, "A", "1", 2));
+        done();
+    });
+
+    test("invalid row placement", (done) => {
+        assert.isFalse(solver.checkRowPlacement(stringPuzzle, "A", "1", 1));
+        done();
+    });
+
+    test("valid column placement", (done) => {
+        assert.isTrue(solver.checkColPlacement(stringPuzzle, "A", "1", 2));
+        done();
+    });
+
+    test("invalid column placement", (done) => {
+        assert.isFalse(solver.checkColPlacement(stringPuzzle, "A", "1", 1));
+        done();
+    });
+
+    test("valid region placement", (done) => {
+        assert.isTrue(solver.checkRegionPlacement(stringPuzzle, "A", "1", 1));
+        done();
+    });
+
+    test("invalid region placement", (done) => {
+        assert.isFalse(solver.checkRegionPlacement(stringPuzzle, "A", "1", 2));
+        done();
+    });
+
+    test("valid puzzle strings pass the solver", (done) => {
+        assert.equal(solver.solve(stringPuzzle), stringComplete);
+        done();
+    });
+
+    test("invalid puzzle strings fail the solver", (done) => {
+        assert.isFalse(
+            solver.solve(
+                "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6...."
+            )
+        );
+        done();
+    });
+
+    test("solver returns the expected solution for an incomplete puzzle", (done) => {
+        assert.equal(solver.solve(stringPuzzle), stringComplete);
+        done();
+    });
 });
